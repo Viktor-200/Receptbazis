085c26a6ccb1d75aa92d34602f3a865f587e06e5
Viktor-200 <mikoviktor777@gmail.com>
2026-01-27 08:46:47 +0100

    Add files via upload

--- users_db.sql
+++ users_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2026 at 10:12 PM
+-- Generation Time: Jan 27, 2026 at 08:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,6 +57,14 @@ CREATE TABLE `receptek` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
+-- Dumping data for table `receptek`
+--
+
+INSERT INTO `receptek` (`id`, `title`, `description`, `image_path`, `uploaded_by`, `created_at`) VALUES
+(2, 'Rakott Krumpli', 'Hozzávalók 3 személyre\r\n\r\n    Krumpli: 1 – 1,2 kg (lehetőleg sárga, főzni való fajta)\r\n\r\n    Tojás: 5-6 darab\r\n\r\n    Kolbász: 15-20 dkg (füstölt, lángolt kolbász a legjobb)\r\n\r\n    Tejföl: 450-500 g (egy nagy pohár)\r\n\r\n    Zsiradék: Kevés vaj vagy zsír a tepsi kikenéséhez\r\n\r\n    Fűszerek: Só, bors, ízlés szerint egy kevés pirospaprika a tejfölbe\r\n\r\nAz elkészítés lépései\r\n\r\n    Előkészítés: A krumplit héjában, sós vízben tedd fel főni. Egy másik edényben főzd keményre a tojásokat (forrástól számítva kb. 9-10 perc). Ha kész, mindkettőt hűtsd le, pucold meg, és szeleteld karikákra.\r\n\r\n    A kolbász: Karikázd fel a kolbászt is. Ha nagyon zsíros, egy serpenyőben kissé kiolvaszthatod a zsírját előre, de nyersen is a rétegek közé mehet.\r\n\r\n    Rétegezés: Egy tűzálló tálat vagy tepsit kenj ki vékonyan zsírral/vajjal.\r\n\r\n        Alulra: Egy réteg krumpli (szórd meg egy kevés sóval).\r\n\r\n        Középre: Jöhet a tojás és a kolbász vegyesen.\r\n\r\n        Locsolás: Kanazz rá a tejfölből (amit előzőleg pici sóval és borssal kikevertél).\r\n\r\n        Tetejére: Zárd le a sort egy újabb réteg krumplival.\r\n\r\n    A befejezés: A maradék tejfölt oszasd el egyenletesen a legfelső krumplirétegen. Sokan szeretik, ha jut rá egy kis reszelt sajt vagy szalonnapörc is, de az eredeti recept enélkül is kiváló.\r\n\r\n    Sütés: 180-200 fokra előmelegített sütőben süsd kb. 30-40 percig, amíg a tejföl aranybarna nem lesz a tetején.', 'uploads/1769498155_rakottkrumpli.jpg', '', '2026-01-27 07:15:55'),
+(3, 'Almás Sütemény', 'Hozzávalók\r\n\r\nA tésztához:\r\n\r\n    Liszt: 500 g finomliszt\r\n\r\n    Zsiradék: 250 g hideg vaj vagy margarin (ettől lesz omlós)\r\n\r\n    Cukor: 100 g porcukor\r\n\r\n    Lazítás: 1 csomag sütőpor, 1 csipet só\r\n\r\n    Kötőanyag: 2 tojássárgája, 2 evőkanál tejföl\r\n\r\nA töltelékhez:\r\n\r\n    Alma: 1,5 – 2 kg (tisztítás előtt mérve, savanykásabb fajta a legjobb)\r\n\r\n    Ízesítés: 150 g cukor (ízlés szerint), 2 teáskanál őrölt fahéj\r\n\r\n    Alap: 2-3 evőkanál zsemlemorzsa vagy darált dió (ez felszívja az alma levét)\r\n\r\nElkészítés folyamata\r\n1. A tészta összeállítása\r\n\r\nA lisztet keverd el a sütőporral, a sóval és a porcukorral. Morzsold el benne a hideg vajat, amíg homokszerű állagot nem kapsz. Add hozzá a tojássárgákat és a tejfölt, majd gyors mozdulatokkal gyúrd össze.\r\n\r\n    Tipp: Ne gyúrd túl sokáig, mert a kezed melegétől megolvad a vaj, és a tészta rágós lesz omlós helyett! Tedd a hűtőbe pihenni, amíg elkészíted az almát.\r\n\r\n2. Az alma előkészítése\r\n\r\nReszeld le az almát nagy lyukú reszelőn. Hagyd állni 10-15 percig, majd alaposan nyomkodd ki a levét. Keverd hozzá a cukrot és a fahéjat. (Sokan szeretik előre megpárolni az almát egy kevés vajon, de nyersen is tökéletes).\r\n3. Rétegezés\r\n\r\nOszd a tésztát két egyenlő részre. Lisztezett felületen nyújtsd ki az első lapot a tepsi méretére, és fektesd bele. Szórd meg egyenletesen a zsemlemorzsával, majd terítsd szét rajta a kinyomkodott almát. Nyújtsd ki a második lapot is, és óvatosan fektesd a tetejére.\r\n4. Sütés\r\n\r\nA felső lapot szurkáld meg villával több helyen, hogy a gőz távozni tudjon. Kend le a tetejét egy felvert tojással a szép színért. 180°C-ra előmelegített sütőben süsd 35-45 percig, amíg a teteje aranybarna nem lesz.', 'uploads/1769498236_almas.jpg', '', '2026-01-27 07:17:16');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -87,7 +95,7 @@ ALTER TABLE `felhasznalok`
 -- AUTO_INCREMENT for table `receptek`
 --
 ALTER TABLE `receptek`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
